8bc3f5785cc1c753ff52c2d535bffa4a7fc96c7f
IgnatGeorgiev <ignat437@gmail.com>
2016-07-11 14:41:06 +0300

    Sidebar has to be responsive now :/

--- sidebar.php
+++ sidebar.php
@@ -14,13 +14,15 @@ if ( ! is_active_sidebar( 'sidebar-2' ) ) {
 	return;
 }
 ?>
-<div class="columns small-2 widgets_wrap">
-	<aside id="secondary" class="widget-area" role="complementary">
-		<?php dynamic_sidebar( 'sidebar-1' ); ?>
-	</aside><!-- #secondary -->
-</div><!-- .columns medium-4 -->
-<div class="columns small-2 widgets_wrap">
-	<aside class="widget-area second-widget" role="complementary">
-		<?php dynamic_sidebar( 'sidebar-2' ); ?>
-	</aside><!-- #secondary -->
+<div class ="columns small-5 sidebar_wrap">
+	<div class="columns small-2 widgets_wrap first_wrap">
+		<aside id="secondary" class="widget-area first-widget" role="complementary">
+			<?php dynamic_sidebar( 'sidebar-1' ); ?>
+		</aside><!-- #secondary -->
+	</div><!-- .columns medium-4 -->
+	<div class="columns small-2 widgets_wrap second_wrap">
+		<aside class="widget-area second-widget" role="complementary">
+			<?php dynamic_sidebar( 'sidebar-2' ); ?>
+		</aside><!-- #secondary -->
+	</div>
 </div>
